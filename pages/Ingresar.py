8dcaf411c9eba3af26b198639c52a2db2073ffec
lflunal <lulopeze@unal.edu.co>
2023-10-18 17:50:14 -0500

    Actualizacion de toda la logica de registro

--- pages/Ingresar.py
+++ pages/Ingresar.py
@@ -1,34 +1,108 @@
-import yaml
-from yaml.loader import SafeLoader
-from pathlib import Path
+# Pagina de inicio de sesion
 
+# Importar librerias necesarias
 import streamlit as st
+import streamlit_extras
 import streamlit_authenticator as stauth
-from streamlit_extras.switch_page_button import switch_page
+import re
+from deta import Deta
 
-file_path = Path(__file__).parent / "usuariosTest.yaml"
-with file_path.open() as file:
-    datos_usuarios = yaml.load(file, Loader=SafeLoader)
+# Almacenamos la key de la base de datos en una constante
+DETA_KEY = "e0qgr2zg4tq_mbZWcCg7iGCpWFBbCy3GGFjEYHdFmZYR"
+
+# Creamos nuestro objeto deta para hacer la conexion a la DB
+deta = Deta(DETA_KEY)
+
+# Realizamos la conexion a la DB
+db = deta.Base("NutribalanceUsers")
+
+# Funcion para registrar usuarios en la DB
+def insertar_usuario(email, username, age, height, password):
+    """Agrega usuarios a la Base de Datos"""
+    return db.put({"key":email, "username": username, "age":age, "height":height, "password":password})
+
+# Funcion que retorna los usuarios registrados
+def fetch_usuarios():
+    """Regresa un diccionario con los usuarios registrados"""
+    # guardamos los datos de la DB en users y retornamos su contenido
+    users = db.fetch()
+    return users.items
+
+# Funcion que retorna los emails de los usuarios registrados
+def get_emails_usuarios():
+    """Regresa una lista con los emails de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    emails = []
+    # filtramos los emails de la DB
+    for user in users.items:
+        emails.append(user["key"])
+    return emails
+
+# Funcion que retorna los nombres de usuario de los usuarios registrados
+def get_usernames_usuarios():
+    """Regresa una lista con los username de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    usernames = []
+    # filtramos los usernames de la DB
+    for user in users.items:
+        usernames.append(user["username"])
+    return usernames
+
+# Funcion que verifica si un email ingresado es valido
+def validar_email(email):
+    """Retorna True si el email ingresado es valido, de lo contrario retorna False"""
+    # Patrones tipicos de un email valido
+    pattern = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}$"
+    pattern1 = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}+\.[a-z]{1,3}$"
+
+    # Verifica si el email ingresado coincide con algun patron definido
+    if re.match(pattern, email) or re.match(pattern1, email):
+        return True
+    return False
+
+# Funcion que verifica si un username ingresado es valido
+def validar_username(username):
+    """Retorna True si el username es valido, de lo contrario, retorna False"""
+    # Se define el patron de un username tipico
+    pattern = "^[a-zA-Z0-9]*$"
+    # Se verifica si el username ingresado coincide con el patron tipico
+    if re.match(pattern, username):
+        return True
+    return False
+
+try:
+    users = fetch_usuarios()
+    emails = get_emails_usuarios()
+    usernames = get_usernames_usuarios()
+    passwords = [user["password"] for user in users]
+
+    credentials = {"usernames" : {}}
+    for index in range(len(emails)):
+        credentials["usernames"][usernames[index]] = {"name" : emails[index], "password" : passwords[index]}
+    
+    Authenticator = stauth.Authenticate(credentials, cookie_name="Streamlit", key="cookiekey", cookie_expiry_days=3)
+
+    email, authentication_status, username = Authenticator.login("Ingresar", "main")
+
+    if username:
+        if username in usernames:
+            if authentication_status:
+                st.write(f"Bienvenido {username}")
+                Authenticator.logout("Cerrar sesion", location="sidebar")
+            elif not authentication_status:
+                st.warning("Contraseña o nombre de usuario incorrectos")
+            else:
+                st.warning("Por favor ingrese todos los campos")
+        else:
+            st.warning("Nombre de usuario no existe, por favor registrese")
 
 
-authenticator = stauth.Authenticate(
-    datos_usuarios["credentials"],
-    datos_usuarios["cookie"]["name"],
-    datos_usuarios["cookie"]["key"],
-    datos_usuarios["cookie"]["expiry_days"]
-)
+except:
+    st.error("Excepcion lanzada")
 
-
-authenticator.login("Ingresar", "main")
-
-
-if st.session_state["authentication_status"]:
-    authenticator.logout("Cerrar sesion", "sidebar", key="unique_key")
-    switch_page("Nutribalance")
-
-elif st.session_state["authentication_status"] is False:
-    st.error("Usuario o contraseña incorrecto(s)")
-
+# Crear pie de pagina con los datos de contacto de los creadores
 footer = """
 <style>
     .footer {
--- pages/Registrarse.py
+++ pages/Registrarse.py
@@ -1,33 +1,130 @@
-import yaml
-from yaml.loader import SafeLoader
-from pathlib import Path
-
+# Importar librerias necesarias
 import streamlit as st
 import streamlit_extras
 import streamlit_authenticator as stauth
+import re
+from deta import Deta
 
-file_path = Path(__file__).parent / "usuariosTest.yaml"
-with file_path.open() as file:
-    datos_usuarios = yaml.load(file, Loader=SafeLoader)
+# Almacenamos la key de la base de datos en una constante
+DETA_KEY = "e0qgr2zg4tq_mbZWcCg7iGCpWFBbCy3GGFjEYHdFmZYR"
 
-authenticator = stauth.Authenticate(
-    datos_usuarios["credentials"],
-    datos_usuarios["cookie"]["name"],
-    datos_usuarios["cookie"]["key"],
-    datos_usuarios["cookie"]["expiry_days"]
-)
+# Creamos nuestro objeto deta para hacer la conexion a la DB
+deta = Deta(DETA_KEY)
 
-if st.session_state["authentication_status"]:
-    authenticator.logout("Cerrar sesion", "sidebar", key="unique_key")
+# Realizamos la conexion a la DB
+db = deta.Base("NutribalanceUsers")
+
+# Funcion para registrar usuarios en la DB
+def insertar_usuario(email, username, age, height, password):
+    """Agrega usuarios a la Base de Datos"""
+    return db.put({"key":email, "username": username, "age":age, "height":height, "password":password})
+
+# Funcion que retorna los usuarios registrados
+def fetch_usuarios():
+    """Regresa un diccionario con los usuarios registrados"""
+    # guardamos los datos de la DB en users y retornamos su contenido
+    users = db.fetch()
+    return users.items
+
+# Funcion que retorna los emails de los usuarios registrados
+def get_emails_usuarios():
+    """Regresa una lista con los emails de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    emails = []
+    # filtramos los emails de la DB
+    for user in users.items:
+        emails.append(user["key"])
+    return emails
+
+# Funcion que retorna los nombres de usuario de los usuarios registrados
+def get_usernames_usuarios():
+    """Regresa una lista con los username de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    usernames = []
+    # filtramos los usernames de la DB
+    for user in users.items:
+        usernames.append(user["username"])
+    return usernames
+
+# Funcion que verifica si un email ingresado es valido
+def validar_email(email):
+    """Retorna True si el email ingresado es valido, de lo contrario retorna False"""
+    # Patrones tipicos de un email valido
+    pattern = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}$"
+    pattern1 = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}+\.[a-z]{1,3}$"
+
+    # Verifica si el email ingresado coincide con algun patron definido
+    if re.match(pattern, email) or re.match(pattern1, email):
+        return True
+    return False
+
+# Funcion que verifica si un username ingresado es valido
+def validar_username(username):
+    """Retorna True si el username es valido, de lo contrario, retorna False"""
+    # Se define el patron de un username tipico
+    pattern = "^[a-zA-Z0-9]*$"
+    # Se verifica si el username ingresado coincide con el patron tipico
+    if re.match(pattern, username):
+        return True
+    return False
+
+# Formulario de registro con los datos que debe ingresar el usuario
+def registro():
+    with st.form(key="registro", clear_on_submit=True):
+        st.subheader("Registrarse")
+        email = st.text_input("Email", placeholder="Ingrese su Email")
+        username = st.text_input("Usuario", placeholder="Ingrese su nombre de usuario")
+        age = st.text_input("Edad (en años)", placeholder="Ingrese su edad en años")
+        height = st.text_input("Altura (en cm sin puntos ni comas)", placeholder="Ingrese su estatura en cm sin puntos ni comas")
+        password = st.text_input("Contraseña", placeholder="Ingrese su contraseña", type="password")
+
+        # Revisar validez de los datos ingresados por el usuario y registro a la DB
+        if email and username and age and height and password:
+            if validar_email(email):
+                if email not in get_emails_usuarios():
+                    if validar_username(username):
+                        if username not in get_usernames_usuarios():
+                            password_encriptada = stauth.Hasher([password]).generate()
+                            insertar_usuario(email, username, age, height, password_encriptada[0])
+                            st.success("Cuenta creada con exito!")
+                        else:
+                            st.warning("Nombre de usuario en uso")
+                    else:
+                        st.warning("Nombre de usuario invalido (solo debe tener letras y numeros)")
+                else:
+                    st.warning("El email ya esta en uso")
+            else:
+                st.warning("Email invalido")
+        else:
+            st.warning("Debe rellenar todos los campos")
+        
+        st.form_submit_button("Registrate")
 
 try:
-    if authenticator.register_user("Registrar Usuario", preauthorization=False):
-        with open(file_path, "w") as file:
-            yaml.dump(datos_usuarios, file, default_flow_style=False)
-        st.success("Usuario registrado")
-except Exception as e:
-    st.error(e)
+    users = fetch_usuarios()
+    emails = get_emails_usuarios()
+    usernames = get_usernames_usuarios()
+    passwords = [user["password"] for user in users]
 
+    credentials = {"usernames" : {}}
+    for index in range(len(emails)):
+        credentials["usernames"][usernames[index]] = {"name" : emails[index], "password" : passwords[index]}
+
+    Authenticator = stauth.Authenticate(credentials, cookie_name="Streamlit", key="cookiekey", cookie_expiry_days=3)
+
+    if st.session_state["authentication_status"]:
+        Authenticator.logout("Cerrar sesion", location="sidebar")
+        st.write("Ya ha iniciado sesion")
+
+    else:
+        registro()
+
+except:
+    st.error("Excepcion lanzada")
+
+# Crear pie de pagina con los datos de contacto de los creadores
 footer = """
 <style>
     .footer {
@@ -49,4 +146,4 @@ footer = """
     Contactenos: <a href="#">lulopeze@unal.edu.co</a> | <a href="#">aramirezsu@unal.edu.co</a></p>
 </div>
 """
-st.markdown(footer,unsafe_allow_html=True)
+#st.markdown(footer,unsafe_allow_html=True)
