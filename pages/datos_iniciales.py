9d8257f3b9ad8e5f6c7e08eda2a24ff423c75920
Luis Lopez <lulopeze@unal.edu.co>
2023-10-29 02:41:55 -0500

    T&C y almacenar calorias
    Ahora el usuario puede leer los terminos y condiciones desde la pagina de registro, debe aceptarlos si desea poder registrarse.
    El usuario ahora, ademas, tambien puede almacenar en la base de datos la cantidad de calorias que consumio en un dia determinado

--- pages/datos_iniciales.py
+++ pages/datos_iniciales.py
@@ -11,11 +11,124 @@ Original file is located at
 import pandas as pd
 import streamlit as st
 #import streamlit_extras
-#import streamlit_authenticator as stauth
-#import re
-#from deta import Deta
 from streamlit_lottie import st_lottie
 import requests
+import re
+import streamlit_authenticator as stauth
+from deta import Deta
+
+# Almacenamos la key de la base de datos en una constante
+DETA_KEY = "e0qgr2zg4tq_mbZWcCg7iGCpWFBbCy3GGFjEYHdFmZYR"
+
+# Creamos nuestro objeto deta para hacer la conexion a la DB
+deta = Deta(DETA_KEY)
+
+# Realizamos la conexion a la DB
+db = deta.Base("NutribalanceUsers")
+
+# Funcion que retorna los usuarios registrados
+def fetch_usuarios():
+    """Regresa un diccionario con los usuarios registrados"""
+    # guardamos los datos de la DB en users y retornamos su contenido
+    users = db.fetch()
+    return users.items
+
+# Funcion que retorna los emails de los usuarios registrados
+def get_emails_usuarios():
+    """Regresa una lista con los emails de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    emails = []
+    # filtramos los emails de la DB
+    for user in users.items:
+        emails.append(user["key"])
+    return emails
+
+# Funcion que retorna la altura de un usuario
+def get_user_height(sel_user):
+    """
+    Regresa la altura de un usuario seleccionado
+    Parametro sel_user: El usuario del que se desea obtener la altura
+    """
+
+    users = db.fetch()
+    height = 0
+    for user in users.items:
+        if user["username"] == sel_user:
+            height = user["height"]
+            return int(height)
+
+# Funcion que retorna la edad de un usuario
+def get_user_age(sel_user):
+    """
+    Regresa la edad de un usuario seleccionado
+    Parametro sel_user: El usuario del que se desea obtener la altura
+    """
+
+    users = db.fetch()
+    age = 0
+    for user in users.items:
+        if user["username"] == sel_user:
+            age = user["age"]
+            return int(age)
+
+# Funcion que retorna los nombres de usuario de los usuarios registrados
+def get_usernames_usuarios():
+    """Regresa una lista con los username de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    usernames = []
+    # filtramos los usernames de la DB
+    for user in users.items:
+        usernames.append(user["username"])
+    return usernames
+
+# Funcion que verifica si un email ingresado es valido
+def validar_email(email):
+    """Retorna True si el email ingresado es valido, de lo contrario retorna False"""
+    # Patrones tipicos de un email valido
+    pattern = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}$"
+    pattern1 = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}+\.[a-z]{1,3}$"
+
+    # Verifica si el email ingresado coincide con algun patron definido
+    if re.match(pattern, email) or re.match(pattern1, email):
+        return True
+    return False
+
+# Funcion que verifica si un username ingresado es valido
+def validar_username(username):
+    """Retorna True si el username es valido, de lo contrario, retorna False"""
+    # Se define el patron de un username tipico
+    pattern = "^[a-zA-Z0-9]*$"
+    # Se verifica si el username ingresado coincide con el patron tipico
+    if re.match(pattern, username):
+        return True
+    return False
+
+# Manejo de posibles errores
+try:
+    # Se almacenan los datos necesarios de la DB
+    users = fetch_usuarios()
+    emails = get_emails_usuarios()
+    usernames = get_usernames_usuarios()
+    passwords = [user["password"] for user in users]
+
+    # Se crea el diccionario credentials necesario para el funcionamiento del autenticador de cuentas
+    credentials = {"usernames" : {}}
+    for index in range(len(emails)):
+        credentials["usernames"][usernames[index]] = {"name" : emails[index], "password" : passwords[index]}
+
+    # Creacion del autenticador
+    Authenticator = stauth.Authenticate(credentials, cookie_name="Streamlit", key="cookiekey", cookie_expiry_days=3)
+
+    # Crear boton de Cerrar sesion si la sesion fue iniciada
+    if st.session_state["authentication_status"]:
+        email, authentication_status, username = Authenticator.login("Ingresar", "main")
+        Authenticator.logout("Cerrar sesion", location="sidebar")
+
+# Informar de que hubo una excepcion en caso de que la haya
+except:
+    st.error("Excepcion lanzada")
 
 # Crear pie de pagina con los datos de contacto de los creadores
 footer = """
@@ -104,11 +217,18 @@ def mostrar_diferencia_calorias(total_calorias_consumidas, total_calorias_quemad
     elif diferencia_calorias < 0:
         st.write(f"Te faltan {int(abs(diferencia_calorias))} calorías para alcanzar tu ingesta calórica diaria. ¡Asegúrate de comer lo suficiente!")
 
-# Solicitar al usuario ingresar peso y altura
+# Solicitar al usuario ingresar peso
 peso = st.number_input("Ingresa tu peso en kilogramos", min_value=0.1)
-altura = st.number_input("Ingresa tu altura en centímetros, "
-                         "Sin puntos ni comas", step=1)
-edad = st.number_input (" Ingrese su edad", step=1)
+
+# Recolectar los datos de un usuario que haya ingresado
+if st.session_state["authentication_status"]:
+    altura = get_user_height(username)
+else:
+    altura = 0
+if st.session_state["authentication_status"]:
+    edad = get_user_age(username)
+else:
+    edad = 0
 
 # Agregar un menú desplegable para seleccionar el sexo
 sexo = st.selectbox("Selecciona tu sexo:", ["Masculino", "Femenino"])
@@ -126,46 +246,6 @@ nivel_actividad = st.selectbox("Selecciona tu nivel de actividad física:", [
     "Muy alta actividad"
 ])
 
-# Verificar si el usuario ha ingresado valores válidos
-if peso > 0 and altura > 0:
-    # Calcular el IMC
-    altura_metros = altura / 100
-    imc = peso / (altura_metros ** 2)
-
-    # Determinar la categoría de IMC basada en el sexo
-    if sexo == "Masculino":
-        if isinstance(imc, str):
-            st.write(imc)
-        else:
-            if imc < 18.5:
-                categoria = "Bajo peso"
-            elif 18.5 <= imc < 24.9:
-                categoria = "Peso normal"
-            elif 24.9 <= imc < 29.9:
-                categoria = "Sobrepeso"
-            else:
-                categoria = "Obesidad"
-            st.write(f"Tu IMC es {imc:.2f}, lo que corresponde a la categoría de "
-         f"{categoria} para hombres.")
-
-
-    elif sexo == "Femenino":
-        if isinstance(imc, str):
-            st.write(imc)
-        else:
-            if imc < 18.5:
-                categoria = "Bajo peso"
-            elif 18.5 <= imc < 24.9:
-                categoria = "Peso normal"
-            elif 24.9 <= imc < 29.9:
-                categoria = "Sobrepeso"
-            else:
-                categoria = "Obesidad"
-            st.write(f"Tu IMC es {imc:.2f}, lo que corresponde a la categoría de "
-         f"{categoria} para mujeres.")
-else:
-    st.write("Por favor, ingresa valores válidos para peso y altura.")
-
 objetivo = st.selectbox("Selecciona tu objetivo:",
                        ["Aumentar masa muscular", "Mantenerse", "Bajar grasa"])
 
@@ -175,16 +255,56 @@ horas_sueno = calcular_horas_de_sueno(hora_sueno, hora_despertar)
 # Mostrar las horas de sueño
 st.write(f"Dormiste durante {horas_sueno} horas")
 
-# Llamada a la función y almacenar el resultado
-calorias_diarias = calcular_calorias_diarias(sexo, peso, altura, edad, nivel_actividad)
-
-# Mostrar el resultado
-st.write(f"Calorías necesarias en un día: {int(calorias_diarias)} calorías")
-
-
 st.write("### Calculadora de Calorías: ")
 # Llamar a la función con los valores adecuados
 # Crear un botón para realizar el cálculo
-st.button("CALCULAR")
+boton_calcular = st.button("CALCULAR")
+if boton_calcular:
+    if st.session_state["authentication_status"]:
+        # Verificar si el usuario ha ingresado valores válidos
+        if peso > 0 and altura > 0:
+            # Calcular el IMC
+            altura_metros = altura / 100
+            imc = peso / (altura_metros ** 2)
+
+            # Determinar la categoría de IMC basada en el sexo
+            if sexo == "Masculino":
+                if isinstance(imc, str):
+                    st.write(imc)
+                else:
+                    if imc < 18.5:
+                        categoria = "Bajo peso"
+                    elif 18.5 <= imc < 24.9:
+                        categoria = "Peso normal"
+                    elif 24.9 <= imc < 29.9:
+                        categoria = "Sobrepeso"
+                    else:
+                        categoria = "Obesidad"
+                    st.write(f"Tu IMC es {imc:.2f}, lo que corresponde a la categoría de "
+                f"{categoria} para hombres.")
+
+
+            elif sexo == "Femenino":
+                if isinstance(imc, str):
+                    st.write(imc)
+                else:
+                    if imc < 18.5:
+                        categoria = "Bajo peso"
+                    elif 18.5 <= imc < 24.9:
+                        categoria = "Peso normal"
+                    elif 24.9 <= imc < 29.9:
+                        categoria = "Sobrepeso"
+                    else:
+                        categoria = "Obesidad"
+                    st.write(f"Tu IMC es {imc:.2f}, lo que corresponde a la categoría de "
+                f"{categoria} para mujeres.")
+        else:
+            st.write("Por favor, ingresa valores válidos para peso y altura.")
+        calorias_diarias = calcular_calorias_diarias(sexo, peso, altura, edad, nivel_actividad)
+
+        # Mostrar el resultado
+        st.write(f"Calorías necesarias en un día: {int(calorias_diarias)} calorías")
+    else:
+        st.warning("Si desea calcular su IMC debe iniciar sesion")
 
 #mostrar_diferencia_calorias(calorias_diarias, total_calorias_quemadas, total_calorias_consumidas)
--- pages/registro_ejercicios.py
+++ pages/registro_ejercicios.py
@@ -12,6 +12,93 @@ import pandas as pd
 import streamlit as st
 from streamlit_lottie import st_lottie
 import requests
+import re
+import streamlit_authenticator as stauth
+from deta import Deta
+
+# Almacenamos la key de la base de datos en una constante
+DETA_KEY = "e0qgr2zg4tq_mbZWcCg7iGCpWFBbCy3GGFjEYHdFmZYR"
+
+# Creamos nuestro objeto deta para hacer la conexion a la DB
+deta = Deta(DETA_KEY)
+
+# Realizamos la conexion a la DB
+db = deta.Base("NutribalanceUsers")
+
+# Funcion que retorna los usuarios registrados
+def fetch_usuarios():
+    """Regresa un diccionario con los usuarios registrados"""
+    # guardamos los datos de la DB en users y retornamos su contenido
+    users = db.fetch()
+    return users.items
+
+# Funcion que retorna los emails de los usuarios registrados
+def get_emails_usuarios():
+    """Regresa una lista con los emails de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    emails = []
+    # filtramos los emails de la DB
+    for user in users.items:
+        emails.append(user["key"])
+    return emails
+
+# Funcion que retorna los nombres de usuario de los usuarios registrados
+def get_usernames_usuarios():
+    """Regresa una lista con los username de cada usuario"""
+    # guardamos los datos de la DB en users
+    users = db.fetch()
+    usernames = []
+    # filtramos los usernames de la DB
+    for user in users.items:
+        usernames.append(user["username"])
+    return usernames
+
+# Funcion que verifica si un email ingresado es valido
+def validar_email(email):
+    """Retorna True si el email ingresado es valido, de lo contrario retorna False"""
+    # Patrones tipicos de un email valido
+    pattern = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}$"
+    pattern1 = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}+\.[a-z]{1,3}$"
+
+    # Verifica si el email ingresado coincide con algun patron definido
+    if re.match(pattern, email) or re.match(pattern1, email):
+        return True
+    return False
+
+# Funcion que verifica si un username ingresado es valido
+def validar_username(username):
+    """Retorna True si el username es valido, de lo contrario, retorna False"""
+    # Se define el patron de un username tipico
+    pattern = "^[a-zA-Z0-9]*$"
+    # Se verifica si el username ingresado coincide con el patron tipico
+    if re.match(pattern, username):
+        return True
+    return False
+
+# Manejo de posibles errores
+try:
+    # Se almacenan los datos necesarios de la DB
+    users = fetch_usuarios()
+    emails = get_emails_usuarios()
+    usernames = get_usernames_usuarios()
+    passwords = [user["password"] for user in users]
+
+    # Se crea el diccionario credentials necesario para el funcionamiento del autenticador de cuentas
+    credentials = {"usernames" : {}}
+    for index in range(len(emails)):
+        credentials["usernames"][usernames[index]] = {"name" : emails[index], "password" : passwords[index]}
+
+    # Creacion del autenticador
+    Authenticator = stauth.Authenticate(credentials, cookie_name="Streamlit", key="cookiekey", cookie_expiry_days=3)
+
+    # Crear boton de Cerrar sesion si la sesion fue iniciada
+    if st.session_state["authentication_status"]:
+        Authenticator.logout("Cerrar sesion", location="sidebar")
+
+# Informar de que hubo una excepcion en caso de que la haya
+except:
+    st.error("Excepcion lanzada")
 
 # Crear pie de pagina con los datos de contacto de los creadores
 footer = """
